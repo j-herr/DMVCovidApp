701271dbcc0761db22539044aa1c875eb6bf7224
calebwagner115 <caleb.wagner115@gmail.com>
2020-10-04 23:04:37 -0400

    Updates

---
+++ precache-manifest.c399e7d49467337f4968521d2a373724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df0bac5349895a7e9704eed7356368d",
+    "url": "/DMVCovidApp/index.html"
+  },
+  {
+    "revision": "59de6a0eafc7ddb7ca4c",
+    "url": "/DMVCovidApp/static/css/main.357e4061.chunk.css"
+  },
+  {
+    "revision": "276d1765e1950a61f057",
+    "url": "/DMVCovidApp/static/js/2.c0ef9206.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/DMVCovidApp/static/js/2.c0ef9206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59de6a0eafc7ddb7ca4c",
+    "url": "/DMVCovidApp/static/js/main.ca2e3636.chunk.js"
+  },
+  {
+    "revision": "db45c2314830ca9c471c",
+    "url": "/DMVCovidApp/static/js/runtime-main.2c3db9ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DMVCovidApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DMVCovidApp/precache-manifest.ca93a73769e65099db914864043465c7.js"
+  "/DMVCovidApp/precache-manifest.c399e7d49467337f4968521d2a373724.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DMVCovidApp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
