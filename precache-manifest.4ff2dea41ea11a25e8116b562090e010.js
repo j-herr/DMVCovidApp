41a24ec645a2554789bc4aaf4401f104c9334661
j-herr <gmu.jherrera@gmail.com>
2020-11-02 19:44:34 -0500

    Revert "Updates"
    This reverts commit 6c3e39976a9f0745d4d71837ccab7e678e9e25ab.

---
+++ precache-manifest.4ff2dea41ea11a25e8116b562090e010.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156eb290a213fc136cdde7ef74d18b74",
+    "url": "/DMVCovidApp/index.html"
+  },
+  {
+    "revision": "399357a09783558a0705",
+    "url": "/DMVCovidApp/static/css/main.357e4061.chunk.css"
+  },
+  {
+    "revision": "123996c0d4bbcb305f78",
+    "url": "/DMVCovidApp/static/js/2.15515713.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/DMVCovidApp/static/js/2.15515713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399357a09783558a0705",
+    "url": "/DMVCovidApp/static/js/main.776f0b33.chunk.js"
+  },
+  {
+    "revision": "db45c2314830ca9c471c",
+    "url": "/DMVCovidApp/static/js/runtime-main.2c3db9ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DMVCovidApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
