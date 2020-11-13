824eb277cd283856f9f55d1673ad4437b1d912d3
melanie <56747714+m3laniesun@users.noreply.github.com>
2020-11-12 21:33:53 -0500

    Adding Vaccine News section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LocateTestCenter from './props/LocateTestCenter'
 import AboutCovid19 from './props/AboutCovid19';
 import Home from './props/Home';
 import EmptyPage from './props/EmptyPage';
+import VaccineNews from './props/VaccineNews';
 import TopNav from './props/TopNav';
 // import {
 //   HashRouter as Router,
@@ -24,7 +25,7 @@ function App() {
             <Route path="/world" exact component={EmptyPage}/>
             <Route path="/locations" exact component={LocateTestCenter} />
             <Route path="/local-data" exact component={EmptyPage} />
-            <Route path="/vaccine" exact component={EmptyPage} />
+            <Route path="/vaccine" exact component={VaccineNews} />
             <Route path="/about-covid19" exact component={AboutCovid19} />
         </div>       
       </Router>    
